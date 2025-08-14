4742177523f9ea3f6c8381344c57e828a236741b
vyshnavi7981 <your_vyshnavi.bhuvanagiri@ardelisys.com>
2025-08-14 15:21:16 +0530

    File transfer2

--- config.py
+++ config.py
@@ -6,16 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TB_ACCOUNTS = {
-    "account1": os.getenv("ACCOUNT1_BASE_URL", "https://thingsboard.cloud"),
-    "account2": os.getenv("ACCOUNT2_BASE_URL", "https://thingsboard.cloud"),
-    "account3": os.getenv("ACCOUNT3_BASE_URL", "https://thingsboard.cloud")
+    "account1": os.getenv("ACCOUNT1_BASE_URL", "https://thingsboard.cloud")
 }
 
 ACCOUNT1_ADMIN_USER = os.getenv("ACCOUNT1_ADMIN_USER")
 ACCOUNT1_ADMIN_PASS = os.getenv("ACCOUNT1_ADMIN_PASS")
-
-ACCOUNT2_ADMIN_USER = os.getenv("ACCOUNT2_ADMIN_USER")
-ACCOUNT2_ADMIN_PASS = os.getenv("ACCOUNT2_ADMIN_PASS")
-
-ACCOUNT3_ADMIN_USER = os.getenv("ACCOUNT3_ADMIN_USER")
-ACCOUNT3_ADMIN_PASS = os.getenv("ACCOUNT3_ADMIN_PASS")
